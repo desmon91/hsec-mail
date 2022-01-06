82bd13674b0394a88c4d2c0073a7ab9a2018268d
Desmond Kristian Siahaan <Desmond_Siahaan@BANPUINDO.co.id>
2022-01-06 12:09:34 +0800

    change using local smtp

--- utils/transporter.ts
+++ utils/transporter.ts
@@ -2,10 +2,10 @@ import nodemailer from "nodemailer";
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.office365.com",
-  port: 587,
+  host: "internalmail.banpuindo.co.id", //lokal: internalmail.banpuindo.co.id docs: smtp.office365.com
+  port: 25, //lokal: 25, docs: 587
   secure: false, // upgrade later with STARTTLS
-  requireTLS: true,
+  requireTLS: false,
   auth: {
     user: process.env.EMAIL_ID,
     pass: process.env.EMAIL_PASSWORD,
