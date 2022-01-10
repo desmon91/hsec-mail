398b4fd586a3d34438dc9f85626d1caced035cf6
Desmond Kristian Siahaan <Desmond_Siahaan@BANPUINDO.co.id>
2022-01-10 08:34:32 +0800

    use smtp.office365.com for live test

--- utils/transporter.ts
+++ utils/transporter.ts
@@ -2,10 +2,10 @@ import nodemailer from "nodemailer";
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-  host: "internalmail.banpuindo.co.id", //lokal: internalmail.banpuindo.co.id docs: smtp.office365.com
-  port: 25, //lokal: 25, docs: 587
+  host: "smtp.office365.com", //lokal: internalmail.banpuindo.co.id docs: smtp.office365.com
+  port: 587, //lokal: 25, docs: 587
   secure: false, // upgrade later with STARTTLS
-  requireTLS: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_ID,
     pass: process.env.EMAIL_PASSWORD,
